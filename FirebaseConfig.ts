c88d4bd9f19b38daf1ba6848d6584b91a84b3ff3
Courtney Fradreck <courtaga@gmail.com>
2025-07-18 11:52:47 +0200

    feat: add FirebaseConfig.ts with environment variables

---
+++ FirebaseConfig.ts
@@ -0,0 +1,48 @@
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getAnalytics, isSupported } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+import { Auth, getAuth, initializeAuth } from 'firebase/auth';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import { Platform } from 'react-native';
+import { getStorage } from 'firebase/storage';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+
+export const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+let auth: Auth;
+
+if (Platform.OS === 'web') {
+  auth = getAuth(app);
+} else {
+  const { getReactNativePersistence } = require('firebase/auth');
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
+}
+
+let analytics;
+if (Platform.OS !== 'web') {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+} else {
+  analytics = getAnalytics(app);
+}
+
+
+export { analytics, auth, db, storage };
